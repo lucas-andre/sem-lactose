72c389f7fac21f08b295d9f9ef1833d6ab60ba90
Lucas André <55907230+lucas-andre@users.noreply.github.com>
2021-08-27 00:43:27 -0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -14,3 +14,18 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active2");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
